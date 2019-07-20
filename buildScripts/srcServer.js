a6b8899ce5dc48c062c450bb00beb73322a2392b
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-07-20 22:02:42 +0300

    Add express server

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,12 +5,12 @@ var open = require('open');
 var port = 3000;
 var app = express;
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
-  if(err) {
+app.listen(port, function (err) {
+  if (err) {
     console.log(err);
   } else {
     open('http://localhost:' + port);
