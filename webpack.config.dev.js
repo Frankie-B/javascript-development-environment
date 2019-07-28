69821f669520e9326e336a6ea96a9f5a590438f2
Frankie Bukenya <frankie.bukenya@gmail.com>
2019-07-28 13:00:19 +0300

    Add html webpack plugin to dev config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -13,11 +14,17 @@ export default {
     publicPath: '/',
     filename: 'bundle.js',
   },
-  plugins: [],
+  plugins: [
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true,
+    })
+  ],
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style', 'css']},
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+      { test: /\.css$/, loaders: ['style', 'css'] },
     ]
   }
 }
